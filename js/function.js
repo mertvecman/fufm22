6fe8453a91a4fd3eaff5b2d9fc22f403ca64090d
kajaha127 <kajaha127@gmail.com>
2023-11-25 11:30:08 +0900

    mobile add

---
+++ js/function.js
@@ -0,0 +1,13 @@
+const $mnu = $("header > nav > ul > li");
+const $sect = $("section");
+
+// $(window).scroll(function () {
+//   let $scl = $(this).scrollTop();
+
+//   $sect.each(function (i) {
+//     let $target = $(this);
+//     if ($target.offset().top < $scl) {
+//       $mnu.addClass("on").siblings().removeClass("on");
+//     }
+//   });
+// });
